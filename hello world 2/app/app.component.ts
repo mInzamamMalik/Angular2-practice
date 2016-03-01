35d0cba26f98bd5a04173dddcac2af6bd4525adb
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-03-01 18:33:48 +0500

    hello world 2 completed

---
+++ hello world 2/app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App,  {{name}} </h1>'
+})
+
+
+
+
+export class AppComponent {
+    name: string;
+
+     constructor() {
+        this.name = 'Felipe';
+    }
+ }
